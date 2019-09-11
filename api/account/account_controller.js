a46fd700b7e7c7cc94f62b6689e4c6babdb177ff
Kateti Mareko <katetimareko@Katetis-MBP.NetComm.Wireless>
2019-09-11 13:37:53 +1200

    Final

--- api/account/account_controller.js
+++ api/account/account_controller.js
@@ -13,6 +13,7 @@ exports.verifyUser = (req, res, next) => {
     AccountService.verifyUser(tokenToVerify).then (info => {
             res.cookie('bokinanene-pub', new Buffer(process.env.pub,'utf8').toString('base64'), 
                 { 
+                    sameSite: true,
                     secure: true,
                     httpOnly: true,
                 })
